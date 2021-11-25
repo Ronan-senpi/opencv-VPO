93e1a3d177e7a71dd6e57cccac588e1520656b00
Ronan <r.dhersignerie@gmail.com>
2021-11-26 00:29:10 +0100

    features step 3

--- Featuers/Featuers.cpp
+++ Featuers/Featuers.cpp
@@ -5,7 +5,8 @@
 #include <string>
 #include <cstdarg>
 
-const float DISTANCE_COEF = 0.6f;
+const float DISTANCE_COEF = 0.75f;
+const int MIN_MATCH_COUNT = 8;
 int compare2Img() {
 
 	cv::Mat leftDescriptor, rightDescriptor;
@@ -60,7 +61,8 @@ int compareImgVid() {
 		system("pause"); //wait for any key press
 		return -1;
 	}
-
+	cv::Mat leftImgG;
+	leftImg.copyTo(leftImgG);
 	cv::Mat rightImg;
 	cv::VideoCapture cap("./img/set1/video.mp4");
 	if (cap.isOpened() == false)
@@ -70,20 +72,24 @@ int compareImgVid() {
 		return -1;
 	}
 
-	auto orb = cv::ORB::create(1000);
+	auto orb = cv::ORB::create(750);
 
-	orb->detect(leftImg, leftKeypoints);
-	orb->compute(leftImg, leftKeypoints, leftDescriptor);
+	orb->detect(leftImgG, leftKeypoints);
+	orb->compute(leftImgG, leftKeypoints, leftDescriptor);
 	while (true)
 	{
+		//Begin: detect points
 		bool bSuccess = cap.read(rightImg);
 		if (bSuccess == false)
 		{
 			std::cout << "Found the end of the video" << std::endl;
 			break;
 		}
-		orb->detect(rightImg, rightKeypoints);
-		orb->compute(rightImg, rightKeypoints, rightDescriptor);
+
+		cv::Mat rightImgG;
+		rightImg.copyTo(rightImgG);
+		orb->detect(rightImgG, rightKeypoints);
+		orb->compute(rightImgG, rightKeypoints, rightDescriptor);
 
 		auto bfm = cv::BFMatcher::create();
 		std::vector<std::vector<cv::DMatch>> matches;
@@ -97,8 +103,41 @@ int compareImgVid() {
 		}
 
 		cv::Mat knnImg;
-		cv:drawMatches(leftImg, leftKeypoints, rightImg, rightKeypoints, good, knnImg);
+	cv:drawMatches(leftImg, leftKeypoints, rightImg, rightKeypoints, good, knnImg);
+		//End: detect points
+		//Begin: Clipping
+		if (good.size() > MIN_MATCH_COUNT)
+		{
+			std::vector<cv::Point2f> obj, scene;
 
+			for (auto i : good) {
+				obj.push_back(leftKeypoints[i.queryIdx].pt);
+				scene.push_back(rightKeypoints[i.trainIdx].pt);
+			}
+			cv::Mat H = cv::findHomography(obj, scene, cv::RANSAC);
+			
+
+			std::vector<cv::Point2f> obj_corners(4);
+			obj_corners[0] = cv::Point2f(0, 0);
+			obj_corners[1] = cv::Point2f((float)leftImg.cols, 0);
+			obj_corners[2] = cv::Point2f((float)leftImg.cols, (float)leftImg.rows);
+			obj_corners[3] = cv::Point2f(0, (float)leftImg.rows);
+			std::vector<cv::Point2f> scene_corners(4);
+
+			cv::perspectiveTransform(obj_corners, scene_corners, H);
+
+			cv::line(knnImg, scene_corners[0] + cv::Point2f((float)leftImg.cols, 0),
+				scene_corners[1] + cv::Point2f((float)leftImg.cols, 0), cv::Scalar(0, 255, 0), 4);
+			cv::line(knnImg, scene_corners[1] + cv::Point2f((float)leftImg.cols, 0),
+				scene_corners[2] + cv::Point2f((float)leftImg.cols, 0), cv::Scalar(0, 255, 0), 4);
+			cv::line(knnImg, scene_corners[2] + cv::Point2f((float)leftImg.cols, 0),
+				scene_corners[3] + cv::Point2f((float)leftImg.cols, 0), cv::Scalar(0, 255, 0), 4);
+			cv::line(knnImg, scene_corners[3] + cv::Point2f((float)leftImg.cols, 0),
+				scene_corners[0] + cv::Point2f((float)leftImg.cols, 0), cv::Scalar(0, 255, 0), 4);
+		}
+		//End: Clipping
+
+		//cv::imshow("Good Matches & Object detection", img_matches);
 		cv::imshow("knnImg", knnImg);
 		if (cv::waitKey(10) == 27)
 		{
