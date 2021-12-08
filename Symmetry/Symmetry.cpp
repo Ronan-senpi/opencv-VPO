dab0282b7e86401aa08c2641533ee410e8f302d2
Ronan <r.dhersignerie@gmail.com>
2021-12-09 00:34:35 +0100

    Symetry step 1

--- Symmetry/Symmetry.cpp
+++ Symmetry/Symmetry.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char** argv)
 {
 	// Read the image file
-	cv::Mat image = cv::imread("D:/Médias/Aedrine-Round.png");
+	cv::Mat image = cv::imread("./img/f.jpg");
 
 	if (image.empty()) // Check for failure
 	{
@@ -14,16 +14,48 @@ int main(int argc, char** argv)
 		system("pause"); //wait for any key press
 		return -1;
 	}
+	cv::Mat fImage;
+	cv::flip(image, fImage, cv::RotateFlags::ROTATE_180);
 
-	std::string windowName = "Symmetry"; //Name of the window
+	cv::Mat descriptor, fDescriptor;
+	std::vector<cv::KeyPoint> keypoints, fKeypoints;
+	auto orb = cv::ORB::create(); 
+	auto bfm = cv::BFMatcher::create();
 
-	cv::namedWindow(windowName); // Create a window
+	orb->detect(image, keypoints);
+	orb->compute(image, keypoints, descriptor);
 
-	cv::imshow(windowName, image); // Show our image inside the created window.
+	orb->detect(fImage, fKeypoints);
+	orb->compute(fImage, fKeypoints, fDescriptor);
+
+
+	std::vector<std::vector<cv::DMatch>> matches;
+	bfm->knnMatch(descriptor, fDescriptor, matches, 2);
+
+	std::vector<cv::DMatch> good;
+	for (auto elem : matches) {
+		if (elem[0].distance < 0.75 * elem[1].distance) {
+			good.push_back(elem[0]);
+		}
+	}
+	cv::Mat knnImg;
+cv:drawMatches(image, keypoints, fImage, fKeypoints, good, knnImg);
+
+	cv::imshow("knnImg", knnImg);
+
+#pragma region Keypoint
+
+	cv::Mat featureImg;
+	drawKeypoints(image, keypoints, featureImg, cv::Scalar::all(-1), cv::DrawMatchesFlags::DEFAULT);
+	cv::Mat fFeatureImg;
+	drawKeypoints(fImage, fKeypoints, fFeatureImg, cv::Scalar::all(-1), cv::DrawMatchesFlags::DEFAULT);
+
+	cv::imshow("windowName", featureImg); // Show our image inside the created window.
+	cv::imshow("fWindowName", fFeatureImg); // Show our image inside the created window.
+
+#pragma endregion Keypoint
 
 	cv::waitKey(0); // Wait for any keystroke in the window
-
-	cv::destroyWindow(windowName); //destroy the created window
 
 	return 0;
 }
