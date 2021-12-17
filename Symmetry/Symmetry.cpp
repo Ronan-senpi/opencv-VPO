3eb0212945df302d6462546b9dca43dc4632cad7
Ronan <r.dhersignerie@gmail.com>
2021-12-17 16:22:57 +0100

    Fix last minutes

--- Symmetry/Symmetry.cpp
+++ Symmetry/Symmetry.cpp
@@ -1,80 +1,83 @@
 #include <opencv2/opencv.hpp>
-
+#include <filesystem>
 #include <iostream>
 #include <string>
 
 int main(int argc, char** argv)
 {
-	// Read the image file
-	cv::Mat image = cv::imread("./img/f.jpg");
+	for (const auto& entry : std::filesystem::directory_iterator("./img/")) {
 
-	if (image.empty()) // Check for failure
-	{
-		std::cout << "Could not open or find the image" << std::endl;
-		system("pause"); //wait for any key press
-		return -1;
-	}
-	cv::Mat fImage;
-	cv::flip(image, fImage, 1);
-	//cv::imshow("flip", fImage);
-	//cv::imshow("normal", image);
-	cv::Mat descriptor, fDescriptor;
-	std::vector<cv::KeyPoint> keypoints, fKeypoints;
-	auto orb = cv::ORB::create(1500);
-	auto bfm = cv::BFMatcher::create();
+		std::string strPath = entry.path().string();
+		if (strPath.substr(strPath.size() - 3) == "jpg")
+		{
 
-	orb->detect(image, keypoints);
-	orb->compute(image, keypoints, descriptor);
+			// Read the image file
+			cv::Mat image = cv::imread(strPath);
 
-	orb->detect(fImage, fKeypoints);
-	orb->compute(fImage, fKeypoints, fDescriptor);
+			if (image.empty()) // Check for failure
+			{
+				std::cout << "Could not open or find the image" << std::endl;
+				system("pause"); //wait for any key press
+				return -1;
+			}
+			cv::Mat fImage;
+			cv::flip(image, fImage, 1);
+			cv::Mat descriptor, fDescriptor;
+			std::vector<cv::KeyPoint> keypoints, fKeypoints;
+			auto orb = cv::ORB::create(10000);
+			auto bfm = cv::BFMatcher::create();
+
+			orb->detect(image, keypoints);
+			orb->compute(image, keypoints, descriptor);
+
+			orb->detect(fImage, fKeypoints);
+			orb->compute(fImage, fKeypoints, fDescriptor);
 
 
-	std::vector<std::vector<cv::DMatch>> matches;
-	bfm->knnMatch(descriptor, fDescriptor, matches, 2);
-	std::vector<cv::Point2f> goodPoints, fGoodPoints, hBarrycenter;
-	for (auto elem : matches) {
-		if (elem[0].distance < 0.8 * elem[1].distance) {
-			cv::Point2f p = keypoints[elem[0].queryIdx].pt;
-			goodPoints.push_back(p);
-			cv::Point2f fp = fKeypoints[elem[0].trainIdx].pt;
-			fp.x = image.cols - fp.x;
-			fGoodPoints.push_back(fp);
-			hBarrycenter.push_back(cv::Point((p.x - fp.x) / 2 + fp.x, (p.y - fp.y) / 2 + fp.y));
+			std::vector<std::vector<cv::DMatch>> matches;
+			bfm->knnMatch(descriptor, fDescriptor, matches, 2);
+			std::vector<cv::Point2f>  fGoodPoints, barrycenters;
+			for (auto elem : matches) {
+				if (elem[0].distance < 0.78 * elem[1].distance) {
+					cv::Point2f p = keypoints[elem[0].queryIdx].pt;
+					cv::Point2f fp = fKeypoints[elem[0].trainIdx].pt;
+					fp.x = image.cols - fp.x;
+					fGoodPoints.push_back(fp);
+					barrycenters.push_back(cv::Point((p.x - fp.x) / 2 + fp.x, (p.y - fp.y) / 2 + fp.y));
+				}
+			}
+
+			cv::Mat bari = cv::Mat::zeros(image.size(), CV_8UC1);
+			for (auto p : barrycenters) {
+				cv::circle(bari, p, 1, cv::Scalar(255, 255, 255));
+			}
+
+
+			std::vector<cv::Vec2f> lines;
+			cv::HoughLines(bari, lines, 3, CV_PI / 180, barrycenters.size() / 3.5, 0, 0);
+			if (!lines.empty()) {
+				float rho = lines[0][0];
+				float theta = lines[0][1];
+				cv::Point pt1, pt2;
+
+				double a = cos(theta);
+				double b = sin(theta);
+				double x0 = a * rho, y0 = b * rho;
+				pt1.x = cvRound(x0 + 1000 * (-b));
+				pt1.y = cvRound(y0 + 1000 * (a));
+				pt2.x = cvRound(x0 - 1000 * (-b));
+				pt2.y = cvRound(y0 - 1000 * (a));
+				cv::line(image, pt1, pt2, cv::Scalar(0, 0, 255), 3, cv::LINE_AA);
+			}
+
+			cv::imshow("barrycenter " + strPath, bari);
+			cv::imshow(strPath, image);
+			
+			cv::waitKey(0); // Wait for any keystroke in the window
+
+			cv::destroyWindow("barrycenter " + strPath);
+			cv::destroyWindow(strPath);
 		}
 	}
-
-	//cv::Mat knnImg;
-//cv:drawMatches(image, keypoints, image, fKeypoints, good, knnImg);
-	//cv::imshow("knnImg", knnImg);
-	//for (size_t i = 0; i < goodPoints.size(); i++) {
-	//	cv::line(image, goodPoints[i], fGoodPoints[i], cv::Scalar(255, 0, 0, 100));
-	//}
-	cv::imshow("image", image);
-	cv::Mat bari = cv::Mat::zeros(image.size(), image.type());
-	for (auto p : hBarrycenter) {
-		cv::circle(image, p, 2, cv::Scalar(0, 255, 0),2);
-		cv::circle(bari, p, 2, cv::Scalar(255, 255, 255), 2);
-	}
-
-
-	cv::imshow("barrycenterImage", image);
-	cv::imshow("barrycenter", bari);
-
-
-#pragma region Keypoint
-
-	//cv::Mat featureImg;
-	//drawKeypoints(image, keypoints, featureImg, cv::Scalar::all(-1), cv::DrawMatchesFlags::DEFAULT);
-	//cv::Mat fFeatureImg;
-	//drawKeypoints(fImage, fKeypoints, fFeatureImg, cv::Scalar::all(-1), cv::DrawMatchesFlags::DEFAULT);
-
-	//cv::imshow("windowName", featureImg); // Show our image inside the created window.
-	//cv::imshow("fWindowName", fFeatureImg); // Show our image inside the created window.
-
-#pragma endregion Keypoint
-
-	cv::waitKey(0); // Wait for any keystroke in the window
-
 	return 0;
 }
